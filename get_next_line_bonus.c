75efc037cc4c65ff487344af7f8600a582ccaf6d
Yaaqoub Charkouk <ycharkou@c2r8p5.1337.ma>
2024-11-16 08:50:04 +0100

    get_next_line

---
+++ get_next_line_bonus.c
@@ -0,0 +1,46 @@
+#include "get_next_line.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+char *read_and_accumulate(int fd, char *accumulation) {
+    char buffer[BUFFER_SIZE + 1];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        buffer[bytes_read] = '\0';
+        accumulation = ft_strjoin(accumulation, buffer);
+        if (ft_strchr(accumulation, '\n') != (size_t)-1)
+            break;
+    }
+    return accumulation;
+}
+
+char *extract_line(char **accumulation) {
+    char *line;
+    size_t newline_pos = ft_strchr(*accumulation, '\n');
+    if (newline_pos != (size_t)-1) {
+        line = ft_substr(*accumulation, 0, newline_pos + 1);
+        char *new_accumulation = ft_strdup(*accumulation + newline_pos + 1);
+        free(*accumulation);
+        *accumulation = new_accumulation;
+    } else {
+        line = ft_strdup(*accumulation);
+        free(*accumulation);
+        *accumulation = NULL;
+    }
+    return line;
+}
+
+char *get_next_line(int fd) {
+    static char *accumulation;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return NULL;
+    accumulation = read_and_accumulate(fd, accumulation);
+    if (!accumulation || !*accumulation) {
+        free(accumulation);
+        accumulation = NULL;
+        return NULL;
+    }
+    return extract_line(&accumulation);
+}
